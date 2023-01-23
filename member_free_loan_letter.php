cdac0f26c11a77579628fbed3527a1af3a32b591
Andi Wijaya <15795593+sikodoq@users.noreply.github.com>
2023-01-23 23:54:41 +0700

    Update member_free_loan_letter.php

--- member_free_loan_letter.php
+++ member_free_loan_letter.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (C) 2007,2008  Arie Nugraha (dicarve@yahoo.com)
  * Some modifications by Drajat Hasan 2017,2019 (drajat@feraproject.wc.lt)
@@ -27,17 +28,17 @@ define('INDEX_AUTH', '1');
 // main system configuration
 require '../../../../sysconfig.inc.php';
 // IP based access limitation
-require LIB.'ip_based_access.inc.php';
+require LIB . 'ip_based_access.inc.php';
 do_checkIP('smc');
 do_checkIP('smc-membership');
 // start the session
-require SB.'admin/default/session.inc.php';
-require SB.'admin/default/session_check.inc.php';
-require SIMBIO.'simbio_GUI/table/simbio_table.inc.php';
-require SIMBIO.'simbio_GUI/form_maker/simbio_form_table_AJAX.inc.php';
-require SIMBIO.'simbio_GUI/paging/simbio_paging.inc.php';
-require SIMBIO.'simbio_DB/datagrid/simbio_dbgrid.inc.php';
-require SIMBIO.'simbio_DB/simbio_dbop.inc.php';
+require SB . 'admin/default/session.inc.php';
+require SB . 'admin/default/session_check.inc.php';
+require SIMBIO . 'simbio_GUI/table/simbio_table.inc.php';
+require SIMBIO . 'simbio_GUI/form_maker/simbio_form_table_AJAX.inc.php';
+require SIMBIO . 'simbio_GUI/paging/simbio_paging.inc.php';
+require SIMBIO . 'simbio_DB/datagrid/simbio_dbgrid.inc.php';
+require SIMBIO . 'simbio_DB/simbio_dbop.inc.php';
 
 // privileges checking
 $can_read = utility::havePrivilege('membership', 'r');
@@ -50,7 +51,7 @@ if (!$can_read) {
 $max_print = 10;
 
 // clean print queue
-if (isset($_GET['action']) AND $_GET['action'] == 'clear') {
+if (isset($_GET['action']) and $_GET['action'] == 'clear') {
     // update print queue count object
     echo '<script type="text/javascript">parent.$(\'#queueCount\').html(\'0\');</script>';
     utility::jsAlert(__('Print queue cleared!'));
@@ -58,7 +59,7 @@ if (isset($_GET['action']) AND $_GET['action'] == 'clear') {
     exit();
 }
 
-if (isset($_POST['itemID']) AND !empty($_POST['itemID']) AND isset($_POST['itemAction'])) {
+if (isset($_POST['itemID']) and !empty($_POST['itemID']) and isset($_POST['itemAction'])) {
     if (!$can_read) {
         die();
     }
@@ -75,7 +76,7 @@ if (isset($_POST['itemID']) AND !empty($_POST['itemID']) AND isset($_POST['itemA
     // card size
     $size = 2;
     // create AJAX request
-    echo '<script type="text/javascript" src="'.JWB.'jquery.js"></script>';
+    echo '<script type="text/javascript" src="' . JWB . 'jquery.js"></script>';
     echo '<script type="text/javascript">';
     // loop array
     foreach ($_POST['itemID'] as $itemID) {
@@ -100,79 +101,83 @@ if (isset($_POST['itemID']) AND !empty($_POST['itemID']) AND isset($_POST['itemA
         utility::jsAlert($msg);
     } else {
         // update print queue count object
-        echo '<script type="text/javascript">parent.$(\'#queueCount\').html(\''.$print_count.'\');</script>';
+        echo '<script type="text/javascript">parent.$(\'#queueCount\').html(\'' . $print_count . '\');</script>';
         utility::jsAlert(__('Selected items added to print queue'));
     }
     exit();
 }
 
 
-function reseter(){
-$handle = fopen(MDLBS.'membership/fll/loanquee.txt', "r");
-  if(!$handle){
-    utility::jsAlert(__('File Not Found!'));
-  } else {
-    $counter = (int) fread($handle,20);
-    fclose ($handle);
-    $counter++;
-    $handle = fopen(MDLBS.'membership/fll/loanquee.txt', "w");
-    fwrite($handle,"0") ;
-    fclose ($handle) ;  
-  }
+function reseter()
+{
+    $handle = fopen(MDLBS . 'membership/fll/loanquee.txt', "r");
+    if (!$handle) {
+        utility::jsAlert(__('File Not Found!'));
+    } else {
+        $counter = (int) fread($handle, 20);
+        fclose($handle);
+        $counter++;
+        $handle = fopen(MDLBS . 'membership/fll/loanquee.txt', "w");
+        fwrite($handle, "0");
+        fclose($handle);
+    }
 }
 
-$show = fopen(MDLBS.'membership/fll/loanquee.txt', "r");
+$show = fopen(MDLBS . 'membership/fll/loanquee.txt', "r");
 if (!$show) {
     utility::jsAlert(__('Can not read file!'));
 } else {
-    $see = (int)fread($show,20);
-    $seepp = $see+1;
-    $sysconf['number'] = substr('0000'.$seepp,-3,3);
-} 
+    $see = (int)fread($show, 20);
+    $seepp = $see + 1;
+    $sysconf['number'] = substr('0000' . $seepp, -3, 3);
+}
 
 // Reset
-if (isset($_GET['action']) AND $_GET['action'] == 'reset') {
+if (isset($_GET['action']) and $_GET['action'] == 'reset') {
     reseter();
     utility::jsAlert('Nomor berhasil direset.');
+    echo '<script type="text/javascript">parent.$(\'#mainContent\').simbioAJAX(parent.$.ajaxHistory[0].url);</script>';
     exit();
 }
 // card pdf download
-if (isset($_GET['action']) AND $_GET['action'] == 'print') {
+if (isset($_GET['action']) and $_GET['action'] == 'print') {
     // update print queue count object
     echo '<script type="text/javascript">parent.$(\'#queueCount\').html(\'0\');</script>';
     // open result in window
-    echo '<script type="text/javascript">top.jQuery.colorbox({href: "'.MWB.'/membership/fll/member_free_loan_letter_pdfgen.php", iframe: true, width: 800, height: 500, title: "'.__('Free Loan Letter Printing').'"})</script>';
+    echo '<script type="text/javascript">top.jQuery.colorbox({href: "' . MWB . '/membership/fll/member_free_loan_letter_pdfgen.php", iframe: true, width: 800, height: 500, title: "' . __('Free Loan Letter Printing') . '"})</script>';
     exit();
 }
 
 ?>
 <fieldset class="menuBox">
-<div class="menuBoxInner printIcon">
-	<div class="per_title">
-    	<h2><?php echo __('Free Loan Letter Printing'); ?></h2>
+    <div class="menuBoxInner printIcon">
+        <div class="per_title">
+            <h2><?php echo __('Free Loan Letter Printing'); ?></h2>
+        </div>
+        <div class="sub_section">
+            <div class="btn-group">
+                <a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=clear" class="notAJAX btn btn-default" style="color: #f00;"><i class="glyphicon glyphicon-trash"></i>&nbsp;<?php echo __('Clear Print Queue'); ?></a>
+                <a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=print" class="notAJAX btn btn-default"><i class="glyphicon glyphicon-print"></i>&nbsp;<?php echo __('Cetak Surat Bebas Peminjaman'); ?></a>
+                <a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=reset" class="notAJAX btn btn-default" style="color: #f00;"><i class="glyphicon glyphicon-trash"></i>&nbsp;<?php echo __('Reset Nomor'); ?></a>
+                <a href="<?php echo MWB; ?>membership/fll/pop_print_settings.php?type=freeloan" class="notAJAX btn btn-default openPopUp" title="<?php echo __('Setelan Surat Bebas Peminjaman'); ?>"><i class="glyphicon glyphicon-wrench"></i></a>
+            </div>
+            <form name="search" action="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php" id="search" method="get" style="display: inline;"><?php echo __('Search'); ?>:
+                <input type="text" name="keywords" size="30" />
+                <input type="submit" id="doSearch" value="<?php echo __('Search'); ?>" class="button" />
+            </form>
+        </div>
+        <div class="infoBox">
+            <?php
+            echo __('Maximum') . ' <font style="color: #f00">' . $max_print . '</font> ' . __('records can be printed at once. Currently there is') . ' '; //mfc
+            if (isset($_SESSION['fll'])) {
+                echo '<font id="queueCount" style="color: #f00">' . count($_SESSION['fll']) . '</font>';
+            } else {
+                echo '<font id="queueCount" style="color: #f00">0</font>';
+            }
+            echo ' ' . __('in queue waiting to be printed.'); //mfc
+            ?>
+        </div>
     </div>
-	<div class="sub_section">
-		<div class="btn-group">
-		<a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=clear" class="notAJAX btn btn-default" style="color: #f00;"><i class="glyphicon glyphicon-trash"></i>&nbsp;<?php echo __('Clear Print Queue'); ?></a>
-		<a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=print" class="notAJAX btn btn-default"><i class="glyphicon glyphicon-print"></i>&nbsp;<?php echo __('Cetak Surat Bebas Peminjaman'); ?></a>
-		<a target="blindSubmit" href="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php?action=reset" class="notAJAX btn btn-default" style="color: #f00;"><i class="glyphicon glyphicon-trash"></i>&nbsp;<?php echo __('Reset Nomor'); ?></a>
-        <a href="<?php echo MWB; ?>membership/fll/pop_print_settings.php?type=freeloan" class="notAJAX btn btn-default openPopUp" title="<?php echo __('Setelan Surat Bebas Peminjaman'); ?>"><i class="glyphicon glyphicon-wrench"></i></a>
-    </div>
-	    <form name="search" action="<?php echo MWB; ?>membership/fll/member_free_loan_letter.php" id="search" method="get" style="display: inline;"><?php echo __('Search'); ?>:
-	    <input type="text" name="keywords" size="30" />
-	    <input type="submit" id="doSearch" value="<?php echo __('Search'); ?>" class="button" />
-	    </form>
-    </div>
-    <div class="infoBox">
-    <?php
-    echo __('Maximum').' <font style="color: #f00">'.$max_print.'</font> '.__('records can be printed at once. Currently there is').' '; //mfc
-    if (isset($_SESSION['fll'])) {
-        echo '<font id="queueCount" style="color: #f00">'.count($_SESSION['fll']).'</font>';
-    } else { echo '<font id="queueCount" style="color: #f00">0</font>'; }
-    echo ' '.__('in queue waiting to be printed.'); //mfc
-    ?>
-    </div>
-</div>
 </fieldset>
 <?php
 /* search form end */
@@ -182,14 +187,16 @@ $table_spec = 'member AS m
     LEFT JOIN mst_member_type AS mt ON m.member_type_id=mt.member_type_id';
 // create datagrid
 $datagrid = new simbio_datagrid();
-$datagrid->setSQLColumn('m.member_id',
-    'm.member_id AS \''.__('Member ID').'\'',
-    'm.member_name AS \''.__('Member Name').'\'',
+$datagrid->setSQLColumn(
+    'm.member_id',
+    'm.member_id AS \'' . __('Member ID') . '\'',
+    'm.member_name AS \'' . __('Member Name') . '\'',
     'member_essay_no AS \'' . __('Nomor Surat') . '\'',
-    'SUBSTRING(`member_essay`, 1, 20) AS \''.__('Judul Skripsi').'\''); // Length of the titile is limit for 20 character
+    'SUBSTRING(`member_essay`, 1, 20) AS \'' . __('Judul Skripsi') . '\''
+); // Length of the titile is limit for 20 character
 $datagrid->setSQLorder('m.last_update DESC');
 // is there any search
-if (isset($_GET['keywords']) AND $_GET['keywords']) {
+if (isset($_GET['keywords']) and $_GET['keywords']) {
     $keyword = $dbs->escape_string(trim($_GET['keywords']));
     $words = explode(' ', $keyword);
     if (count($words) > 1) {
@@ -213,13 +220,13 @@ $datagrid->edit_property = false;
 $datagrid->chbox_property = array('itemID', __('Add'));
 $datagrid->chbox_action_button = __('Add To Print Queue');
 $datagrid->chbox_confirm_msg = __('Add to print queue?');
-$datagrid->column_width = array('10%', '70%', '15%');
+$datagrid->column_width = array('8%', '15%', '8%', '70%');
 // set checkbox action URL
 $datagrid->chbox_form_URL = $_SERVER['PHP_SELF'];
 // put the result into variables
 $datagrid_result = $datagrid->createDataGrid($dbs, $table_spec, 20, $can_read);
-if (isset($_GET['keywords']) AND $_GET['keywords']) {
-    echo '<div class="infoBox">'.__('Found').' '.$datagrid->num_rows.' '.__('from your search with keyword').': "'.$_GET['keywords'].'"</div>'; //mfc
+if (isset($_GET['keywords']) and $_GET['keywords']) {
+    echo '<div class="infoBox">' . __('Found') . ' ' . $datagrid->num_rows . ' ' . __('from your search with keyword') . ': "' . $_GET['keywords'] . '"</div>'; //mfc
 }
 echo $datagrid_result;
 /* main content end */
